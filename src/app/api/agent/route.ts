ff8eb8fdbd233e980d3203459f40134fa42d2ba5
Christian Sánchez <christiansanchezh@pacifico.com.pe>
2026-03-16 22:17:48 -0500

    Update route.ts

--- src/app/api/agent/route.ts
+++ src/app/api/agent/route.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
   // recrea la URL de tu API remota usando los mismos parámetros
-  const url = `[api]?` +
+  const url = `https://apicloudia-293460116750.us-west4.run.app/agent?` +
     new URL(request.url).searchParams.toString();
 
   // forward
